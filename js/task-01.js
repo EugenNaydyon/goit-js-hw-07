612892aa595856dbfc8bcb71c0c36161b894a0d4
EugenNaydyon <eugennaydyon@gmail.com>
2021-07-03 14:58:05 +0300

    copied first task

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+const categoriesEl = document.querySelectorAll(".item");
+console.log(`В списке ${categoriesEl.length} категории`);
+
+// console.log(document.querySelectorAll("h2"));
+
+const navEl = document.querySelector(".ul");
+console.log(navEl);
+
+// const categoriesFirstLi = Array.from(
+//   document.querySelectorAll(".categories > li")
+// );
+// const categoriesUl = Array.from(
+//   document.querySelectorAll(".categories  li ul")
+// );
+// const categoriesList = [];
+// categoriesFirstLi.forEach((element) => {
+//   categoriesList.push(
+//     `Категория: ${element.firstChild.textContent.trim()} / Количество: ${
+//       element.firstElementChild.children.length
+//     }`
+//   );
+// });
+// console.log(categoriesList);
