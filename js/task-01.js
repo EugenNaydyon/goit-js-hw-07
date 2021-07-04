7d760675c602345822d4f3c6257f9d4a0a32906d
EugenNaydyon <eugennaydyon@gmail.com>
2021-07-04 09:56:06 +0300

    work with mistackes

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,5 @@
 const categoriesListRef = document.querySelector("#categories");
-
 const categoriesListItemRef = categoriesListRef.querySelectorAll(".item");
-
 console.log(`В списке ${categoriesListItemRef.length} категории.`);
 categoriesListItemRef.forEach((category) => {
   console.log("=========================");
