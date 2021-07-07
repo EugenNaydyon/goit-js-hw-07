146e62ace53ab0597cd4ee1569271d98890e3c83
EugenNaydyon <eugennaydyon@gmail.com>
2021-07-08 00:53:48 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,15 @@
-const controlFont = document.getElementById("font-size-control");
-const text = document.getElementById("text");
-controlFont.oninput = function () {
-  text.style.fontSize = controlFont.value + "px";
-};
+const inputRef = document.querySelector("#font-size-control");
+const textFontSizeChangeByInputRef = document.querySelector("#text");
+textFontSizeChangeByInputRef.style.fontSize = `${inputRef.value}px`;
+inputRef.addEventListener("input", onInputRefChange);
+function onInputRefChange(event) {
+  textFontSizeChangeByInputRef.style.fontSize = `${event.currentTarget.value}px`;
+}
+
+// ========================
+
+// const controlFont = document.getElementById("font-size-control");
+// const text = document.getElementById("text");
+// controlFont.oninput = function () {
+//   text.style.fontSize = controlFont.value + "px";
+// };
